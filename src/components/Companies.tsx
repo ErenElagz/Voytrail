42e838923751642592d6edfaba267ae1a98ef22a
ErenElagz <erenelagoz@gmail.com>
2025-11-22 11:38:30 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -8,13 +8,37 @@ export default class Companies extends Component {
     return (
       <div className=" flex flex-col items-center w-7xl max-w-full mx-auto mt-10 gap-5">
         <div className="flex items-center">
-          <span className="ml-2 text-3xl font-black text-sky-800">Trusted by Hostels and Hotels</span>
+          <span className="text-3xl underline font-bold tracking-tight text-black">Trusted by Hostels and Hotels</span>
         </div>
         <div className="flex justify-center items-center gap-10 mt-6 mb-10">
-          <Image src="/funker.png" alt="Funker" width={160} height={80} className="object-contain grayscale hover:opacity-100 transition opacity-50" />
-          <Image src="/elgranado.png" alt="El Granado" width={160} height={80} className="object-contain grayscale hover:opacity-100 transition opacity-50" />{' '}
-          <Image src="/funker.png" alt="Funker" width={160} height={80} className="object-contain grayscale hover:opacity-100 transition opacity-50" />
-          <Image src="/elgranado.png" alt="El Granado" width={160} height={80} className="object-contain grayscale hover:opacity-100 transition opacity-50" />
+          <Image
+            src="/funker.png"
+            alt="Funker"
+            width={160}
+            height={80}
+            className="object-contain grayscale hover:opacity-100 transition opacity-30 duration-500"
+          />
+          <Image
+            src="/elgranado.png"
+            alt="El Granado"
+            width={160}
+            height={80}
+            className="object-contain grayscale hover:opacity-100 transition opacity-30 duration-500"
+          />
+          <Image
+            src="/funker.png"
+            alt="Funker"
+            width={160}
+            height={80}
+            className="object-contain grayscale hover:opacity-100 transition opacity-30 duration-500"
+          />
+          <Image
+            src="/elgranado.png"
+            alt="El Granado"
+            width={160}
+            height={80}
+            className="object-contain grayscale hover:opacity-100 transition opacity-30 duration-500"
+          />
         </div>
       </div>
     );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,31 +6,42 @@ import Image from 'next/image';
 export default class NavBar extends Component {
   render() {
     return (
-      <div className="w-6xl  sticky top-2 z-50 transition my-2 bg-opacity-80 mx-4 rounded-full bg-zinc-900 text-white flex items-center justify-between px-2 py-2 shadow-md">
+      <div className="w-5xl backdrop-blur-md border-2 border-black/5 sticky top-2 z-50 transition my-2 mx-4 rounded-full bg-white/70 text-white flex items-center justify-between p-2">
         <div className="flex items-center w-full">
-          <Image src="/logo.png" alt="Voytrail Logo" width={48} height={48} className="rounded-full" />
-          <span className="ml-2 text-2xl font-bold text-zinc-200">voytrail.</span>
+          <Image src="/logo-white.png" alt="Voytrail Logo" width={40} height={40} className="rounded-full" />
+          <span className="ml-2 text-2xl font-bold text-zinc-700">Voytrail.</span>
         </div>
         <nav className="items-center gap-4">
-          <a href="#" className="px-3 py-2 text-sm font-medium text-zinc-400 hover:text-white transition">
+          <a
+            href="#"
+            className="px-3 py-2 text-md font-medium tracking-tight text-zinc-500 hover:text-zinc-900 transition duration-300"
+          >
             Home
           </a>
-          <a href="#" className="px-3 py-2 text-sm font-medium text-zinc-400 hover:text-white transition">
-            Contact
+          <a
+            href="#"
+            className="px-3 py-2 text-md font-medium tracking-tight text-zinc-500 hover:text-zinc-900 transition duration-300"
+          >
+            Solutions
           </a>
-          <a href="#" className="px-3 py-2 text-sm font-medium text-zinc-400 hover:text-white transition">
+          <a
+            href="#"
+            className="px-3 py-2 text-md font-medium tracking-tight text-zinc-500 hover:text-zinc-900 transition duration-300"
+          >
             Pricing
           </a>
-          <a href="#" className="px-3 py-2 text-sm font-medium text-zinc-400 hover:text-white transition">
-            Service
+          <a
+            href="#"
+            className="px-3 py-2 text-md font-medium tracking-tight text-zinc-500 hover:text-zinc-900 transition duration-300"
+          >
+            Contact
           </a>
         </nav>
         <div className="flex w-full justify-end">
-          <button className="hover:underline transition text-sm text-white font-medium py-3 px-5 rounded-full">
+          <button className="tracking-tight text-zinc-500 hover:text-zinc-900 transition duration-300 text-md font-medium px-4 py-2">
             Get Demo
           </button>
-          <button className="bg-zinc-800 hover:bg-zinc-700 text-sm text-white font-medium py-3 px-5 rounded-full transition">
-            <Icon icon="mdi:login" width="20" height="20" className="inline-block mr-2" />
+          <button className="bg-zinc-800 hover:bg-zinc-700 text-md text-white font-medium px-6 py-2 rounded-full transition">
             Login
           </button>
         </div>
